9f64102d1dc1a87137b82ab08902dbcee85f056c
Haitham Ennasr <haitham@llcl.lc>
2017-03-17 12:26:59 -0400

    Added article post type

--- palfest-post-types.php
+++ palfest-post-types.php
@@ -36,5 +36,38 @@ function palfest_create_palfestivians() {
 		)
 	);
 }
+
+function palfest_create_articles() {
+    // set up labels
+    $labels = array(
+        'name' => 'Articles',
+        'singular_name' => 'Article',
+        'add_new' => 'Add New Article',
+        'add_new_item' => 'Add New Article',
+        'edit_item' => 'Edit Article',
+        'new_item' => 'New Article',
+        'all_items' => 'All Article',
+        'view_item' => 'View Article',
+        'search_items' => 'Search Articles',
+        'not_found' =>  'No Articles Found',
+        'not_found_in_trash' => 'No Articles found in Trash', 
+        'parent_item_colon' => '',
+        'menu_name' => 'Articles',
+    );
+    //register post type
+    register_post_type( 'article', array(
+        'labels' => $labels,
+        'has_archive' => true,
+        'public' => true,
+        'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail','page-attributes' ),
+        'taxonomies' => array( 'post_tag', 'category' ),    
+        'exclude_from_search' => false,
+        'capability_type' => 'post',
+        'rewrite' => array( 'slug' => 'Articles' ),
+        )
+    );
+}
+
 add_action( 'init', 'palfest_create_palfestivians' );
+add_action( 'init', 'palfest_create_articles' );
 ?>
