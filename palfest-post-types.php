22a748ccec84b91f49ca05228eec934fc00444fd
Haitham Ennasr <haitham@llcl.lc>
2017-03-20 15:17:50 -0400

    added post-class metabox example

--- palfest-post-types.php
+++ palfest-post-types.php
@@ -99,7 +99,70 @@ function palfest_create_programme() {
     );
 }
 
+
+/* Meta box setup function. */
+function smashing_post_meta_boxes_setup() {
+
+  /* Add meta boxes on the 'add_meta_boxes' hook. */
+  add_action( 'add_meta_boxes', 'smashing_add_post_meta_boxes' );
+
+  /* Save post meta on the 'save_post' hook. */
+  add_action( 'save_post', 'smashing_save_post_class_meta', 10, 2 );
+}
+
+/* Display the post meta box. */
+function smashing_post_class_meta_box( $object, $box ) { ?>
+
+  <?php wp_nonce_field( basename( __FILE__ ), 'smashing_post_class_nonce' ); ?>
+
+  <p>
+    <label for="smashing-post-class"><?php _e( "Add a custom CSS class, which will be applied to WordPress' post class.", 'example' ); ?></label>
+    <br />
+    <input class="widefat" type="text" name="smashing-post-class" id="smashing-post-class" value="<?php echo esc_attr( get_post_meta( $object->ID, 'smashing_post_class', true ) ); ?>" size="30" />
+  </p>
+
+<?php }
+
+/* Meta box setup function. */
+function smashing_post_meta_boxes_setup() {
+
+  /* Add meta boxes on the 'add_meta_boxes' hook. */
+  add_action( 'add_meta_boxes', 'smashing_add_post_meta_boxes' );
+
+  /* Save post meta on the 'save_post' hook. */
+  add_action( 'save_post', 'smashing_save_post_class_meta', 10, 2 );
+}
+
+/* Filter the post class hook with our custom post class function. */
+add_filter( 'post_class', 'smashing_post_class' );
+
+function smashing_post_class( $classes ) {
+
+  /* Get the current post ID. */
+  $post_id = get_the_ID();
+
+  /* If we have a post ID, proceed. */
+  if ( !empty( $post_id ) ) {
+
+    /* Get the custom post class. */
+    $post_class = get_post_meta( $post_id, 'smashing_post_class', true );
+
+    /* If a post class was input, sanitize it and add it to the post class array. */
+    if ( !empty( $post_class ) )
+      $classes[] = sanitize_html_class( $post_class );
+  }
+
+  return $classes;
+}
+
+
 add_action( 'init', 'palfest_create_programme' );
 add_action( 'init', 'palfest_create_palfestivians' );
 add_action( 'init', 'palfest_create_articles' );
+
+/* Fire our meta box setup function on the post editor screen. */
+add_action( 'load-post.php', 'smashing_post_meta_boxes_setup' );
+add_action( 'load-post-new.php', 'smashing_post_meta_boxes_setup' );
+
+
 ?>
