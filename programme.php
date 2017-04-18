cadb8fbf9e00fdff22366908c9fc617febfe77a5
Haitham Ennasr <haitham@llcl.lc>
2017-04-18 10:30:03 -0400

    split plugin into multiple files

---
+++ programme.php
@@ -0,0 +1,32 @@
+<?php
+function palfest_create_programme() {
+    // set up labels
+    $labels = array(
+        'name' => 'Programme',
+        'singular_name' => 'Day',
+        'add_new' => 'Add New Day',
+        'add_new_item' => 'Add New Day',
+        'edit_item' => 'Edit Day',
+        'new_item' => 'New Day',
+        'all_items' => 'All Days',
+        'view_item' => 'View Day',
+        'search_items' => 'Search Programme Days',
+        'not_found' =>  'No Programme Days Found',
+        'not_found_in_trash' => 'No Programme Days found in Trash', 
+        'parent_item_colon' => '',
+        'menu_name' => 'Programme',
+    );
+    //register post type
+    register_post_type( 'day', array(
+        'labels' => $labels,
+        'has_archive' => true,
+        'public' => true,
+        'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail','page-attributes' ),
+        'taxonomies' => array( 'post_tag', 'category' ),    
+        'exclude_from_search' => false,
+        'capability_type' => 'post',
+        'rewrite' => array( 'slug' => 'Programme' ),
+        )
+    );
+}
+?>
