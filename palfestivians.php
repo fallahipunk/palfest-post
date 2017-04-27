29c0437599eef9308c898cb2e946d3be4b4b77e0
Haitham Ennasr <haitham@llcl.lc>
2017-04-27 13:45:13 -0400

    added post2post connection to peofiles

--- palfestivians.php
+++ palfestivians.php
@@ -77,16 +77,6 @@ function palfestivian_last_name_meta_box( $post ) { ?>
   </p>
 <?php }
 
-function palfestivian_featured_meta_box( $post ) { ?>
-
-  <?php wp_nonce_field( basename( __FILE__ ), 'palfestivian_featured_nonce' ); ?>
-
-  <p>
-    <label for="palfestivian-featured"><?php _e( "Add featured content by this author", 'example' ); ?></label>
-    <br />
-    <input class="widefat" type="text" name="palfestivian-featured" id="palfestivian-featured" value="<?php echo esc_attr( get_post_meta( $post->ID, 'palfestivian_featured', true ) ); ?>" size="30" />
-  </p>
-<?php }
 
 /* Save the last name metadata. */
 
@@ -125,68 +115,16 @@ function palfestivian_save_last_name_meta( $post_id, $post ) {
     delete_post_meta( $post_id, $meta_key, $meta_value );
 }
 
-
-/* Save the featured content metadata. */
-function palfestivian_save_featured_meta( $post_id, $post ) {
-
-  /* Verify the nonce before proceeding. */
-  if ( !isset( $_POST['palfestivian_featured_nonce'] ) || !wp_verify_nonce( $_POST['palfestivian_featured_nonce'], basename( __FILE__ ) ) )
-    return $post_id;
-
-  /* Get the post type object. */
-  $post_type = get_post_type_object( $post->post_type );
-
-  /* Check if the current user has permission to edit the post. */
-  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
-    return $post_id;
-
-  /* Get the posted data and sanitize it for use as an HTML class. */
-  $new_meta_value = ( isset( $_POST['palfestivian-featured'] ) ? sanitize_html_class( $_POST['palfestivian-featured'] ) : '' );
-
-  /* Get the meta key. */
-  $meta_key = 'palfestivian_featured';
-
-  /* Get the meta value of the custom field key. */
-  $meta_value = get_post_meta( $post_id, $meta_key, true );
-
-  /* If a new meta value was added and there was no previous value, add it. */
-  if ( $new_meta_value && '' == $meta_value )
-    add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-
-  /* If the new meta value does not match the old value, update it. */
-  elseif ( $new_meta_value && $new_meta_value != $meta_value )
-    update_post_meta( $post_id, $meta_key, $new_meta_value );
-
-  /* If there is no new meta value but an old value exists, delete it. */
-  elseif ( '' == $new_meta_value && $meta_value )
-    delete_post_meta( $post_id, $meta_key, $meta_value );
+// ------------ link profile to articles, requires post2post -----------------//
+function my_connection_types() {
+    p2p_register_connection_type( array(
+        'name' => 'profiles_to_articless',
+        'from' => 'profile',
+        'to' => 'article'
+    ) );
 }
+add_action( 'p2p_init', 'my_connection_types' );
 
-
-//////////////////////////////////////////////////////////////
-
-
-/* Filter the post class hook with our custom post class function. */
-add_filter( 'post_class', 'palfestivian_featured' );
-
-function palfestivian_featured( $classes ) {
-
-  /* Get the current post ID. */
-  $post_id = get_the_ID();
-
-  /* If we have a post ID, proceed. */
-  if ( !empty( $post_id ) ) {
-
-    /* Get the custom post class. */
-    $post_class = get_post_meta( $post_id, 'palfestivian_featured', true );
-
-    /* If a post class was input, sanitize it and add it to the post class array. */
-    if ( !empty( $post_class ) )
-      $classes[] = sanitize_html_class( $post_class );
-  }
-
-  return $classes;
-}
 
 
 ////////////////////////////////////////////////////////////
